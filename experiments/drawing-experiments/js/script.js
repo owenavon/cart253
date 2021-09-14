59b6955bc9434653358e32547541bcb1a6267972
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-14 15:50:45 -0400

    drawing experiment with colour and transparency.

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -11,16 +11,17 @@ author, and this description to match your project!
 /**
 Description of preload
 */
-function preload() {
-
-}
+function preload() {}
 
 /**
 Description of setup
 */
+
 function setup() {
   createCanvas(500, 500); // Canvas size - 500 X 500 pixels
-  background(255, 138, 1); // Background colour: Orange
+
+  background(191, 255, 199); // r, g, b
+
   // point(250, 250); // Function - arguments go inside parentheses
   // rect(0, 0, 250, 250); // Drawn relative to the top left
   // line(0, 0, 500, 500); // Top left to bottom right
@@ -33,25 +34,31 @@ function setup() {
   // rect (250, 250, 60, 60);
 
   ellipseMode(CORNER);
+
+  noStroke();
+
+  fill(127, 0, 200, 100);
   ellipse(250, 250, 100, 100);
+
+  fill(137, 0, 210, 100);
   ellipse(250, 250, 80, 80);
+
+  fill(147, 0, 220, 100);
   ellipse(250, 250, 60, 60);
+
+  fill(157, 0, 230, 100);
   ellipse(250, 250, 40, 40);
+
+  fill(167, 0, 240, 100);
   ellipse(250, 250, 20, 20);
 
   // triangle(200, 200, 300, 100, 400, 200); // Left coordinate, top coordinate, right coordinate
-  //
   // strokeWeight(10);
   // stroke(150);
   // line(100, 25, 400, 100);
-
-
-
 }
 
 /**
 Description of draw()
 */
-function draw() {
-
-}
+function draw() {}
