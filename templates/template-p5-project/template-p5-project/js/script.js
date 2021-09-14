9615857c423548d438f58c47c1b08bed9dbf88c7
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-14 12:09:24 -0400

    Commit after following along to "2.1, Working with code in Atom"

--- templates/template-p5-project/template-p5-project/js/script.js
+++ templates/template-p5-project/template-p5-project/js/script.js
@@ -1,13 +1,12 @@
 /**
-Title of Project
-Author Name
+Template p5 project
+Owen Avon
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
 "use strict";
-
 
 /**
 Description of preload
@@ -16,14 +15,12 @@ function preload() {
 
 }
 
-
 /**
 Description of setup
 */
 function setup() {
-
+  background (127, 255, 200);
 }
-
 
 /**
 Description of draw()
