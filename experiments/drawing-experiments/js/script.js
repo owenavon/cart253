1b4644aebb84083f89b9fe4840bda660eedb7a90
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-15 15:23:10 -0400

    Drawing experiment: added comments

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,34 +1,44 @@
-/**
+/********************************************************
 drawing-experiments - Exercise
 Owen Avon
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+Experimenting with p5's drawing and colour functions.
+*********************************************************/
 
 "use strict";
 
-/**
-Description of preload
-*/
+// preload()
+//
+// Does nothing.
 function preload() {}
 
-/**
-Description of setup
-*/
-
+// setup()
+//
+// Draws a face on the canvas
 function setup() {
-  createCanvas(500, 500); // Canvas size - 500 X 500 pixels
+  // Canvas size - 500 X 500 pixels
+  createCanvas(500, 500);
 
-  background(191, 255, 199); // r, g, b
+  // r, g, b
+  background(191, 255, 199);
 
-  // point(250, 250); // Function - arguments go inside parentheses
-  // rect(0, 0, 250, 250); // Drawn relative to the top left
-  // line(0, 0, 500, 500); // Top left to bottom right
-  // line(500, 0, 0, 500); // Top right to bottom left
-  // ellipse(250, 250, 100, 100); // x, y, w, h - Drawn realtive to the center
+  // Function - arguments go inside parentheses
+  // point(250, 250);
 
-  // rectMode(CENTER); // Drawn releative from the cnetre of the rectangle
+  // Drawn relative to the top left
+  // rect(0, 0, 250, 250);
+
+  // Top left to bottom right
+  // line(0, 0, 500, 500);
+
+  // Top right to bottom left
+  // line(500, 0, 0, 500);
+
+  // x, y, w, h - Drawn realtive to the center
+  // ellipse(250, 250, 100, 100);
+
+  // Drawn releative from the cnetre of the rectangle
+  // rectMode(CENTER);
   // rect(250, 250, 100, 100);
   // rect (250, 250, 80, 80);
   // rect (250, 250, 60, 60);
@@ -37,28 +47,36 @@ function setup() {
 
   noStroke();
 
+  // First cone
   fill(127, 0, 200, 100);
   ellipse(250, 250, 100, 100);
 
+  // Second cone
   fill(137, 0, 210, 100);
   ellipse(250, 250, 80, 80);
 
+  // Third cone
   fill(147, 0, 220, 100);
   ellipse(250, 250, 60, 60);
 
+  // Fouth cone
   fill(157, 0, 230, 100);
   ellipse(250, 250, 40, 40);
 
+  // Fifth cone
   fill(167, 0, 240, 100);
   ellipse(250, 250, 20, 20);
 
-  // triangle(200, 200, 300, 100, 400, 200); // Left coordinate, top coordinate, right coordinate
+  // triangle(200, 200, 300, 100, 400, 200);
+  // Left coordinate, top coordinate, right coordinate
   // strokeWeight(10);
   // stroke(150);
   // line(100, 25, 400, 100);
 }
 
-/**
-Description of draw()
-*/
-function draw() {}
+// draw()
+//
+// Does nothing.
+function draw() {
+
+}
