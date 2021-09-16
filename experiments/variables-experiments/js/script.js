5c2db65f0d39271e20b765962dae0d1868618066
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-16 17:04:34 -0400

    Learned about changing variables

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -8,9 +8,11 @@ This exercise introduces variables
 "use strict";
 
 let backgroundShade = 0;
-let circleX = 250;
+let circleX = 0;
 let circleY = 250;
-let circleSize = 100;
+let circleSize = 200;
+let circleSpeed = 2;
+let circleAcceleration = 0.25;
 
 // setup()
 //
@@ -22,10 +24,16 @@ function setup() {
 
 // draw()
 //
-// Description of draw() goes here.
+// Draw() draws the functions at 60 FPS.
 function draw() {
   background(backgroundShade);
+  circleX = circleX + circleSpeed;
+  circleSpeed = circleSpeed + circleAcceleration; // optional shortform. circleSpeed += circleAcceleration;
   ellipse(circleX, circleY, circleSize);
+
+  // backgroundShade = backgroundShade + 1;
+  // circleSize = circleSize * 1.01;
+  // circleY = circleY / 1.01;
   // rect(mouseX, mouseY);
   // rectMode(CENTER);
   // rect(width / 2, height / 2, 100, 100);
