a4ccb5c682c0da6ce550284d5289e9e6936db641
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-21 22:42:22 -0400

    E1: Started exercise 1

---
+++ exercises/i-like-to-move-it/js/script.js
@@ -0,0 +1,24 @@
+/**
+i-like-to-move-it
+Owen Avon
+
+This program explores the animation shapes and backgrounds.
+*/
+
+"use strict";
+
+//setup();
+//
+// Description of setup() goes here.
+
+function setup() {
+
+}
+
+//draw();
+//
+// Description of draw() goes here.
+
+function draw() {
+
+}
