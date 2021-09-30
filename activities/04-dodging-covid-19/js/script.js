d68995513cdea41d5e2e5d98eb5ac330b0034098
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-30 15:19:34 -0400

    A4: Started "Dodging COVID-19"

---
+++ activities/04-dodging-covid-19/js/script.js
@@ -0,0 +1,17 @@
+
+// Dodging COVID-19
+// Owen Avon
+
+// Exercise 04 - Working with Conditionals, 
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
