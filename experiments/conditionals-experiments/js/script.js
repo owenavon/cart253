71cc7673f8a20a2541e3b9ad6e636fff31c52108
owenavon <89999663+owenavon@users.noreply.github.com>
2021-09-30 14:44:21 -0400

    Started experimenting with intermediate drawing

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -22,16 +22,22 @@
 
 // let displayCircle = false; // Don't display circle untill we click on canvas.
 
-let bg = {
-  r: 0,
-  g: 0,
-  b: 0,
-}
+// let bg = {
+//   r: 0,
+//   g: 0,
+//   b: 0,
+// }
 
 let circle = {
   x: 250,
   y: 250,
-  size: 100
+  size: 100,
+  vx: 0,
+  vy: 0,
+  ax: 0,
+  ay: 0,
+  acceleration: 0.25,
+  maxSpeed: 5
 }
 
 // Description of setup()
@@ -45,17 +51,37 @@ function setup() {
 // Description of draw()
 function draw() {
   background(0);
-  background(bg.r, bg.g, bg.b);
 
+  if (mouseX < circle.x) {
+    circle.ax = -circle.acceleration;
+  }
+  else {
+    circle.ax = circle.acceleration;
+  }
 
-ellipse(circle.x, circle.y, circle.size);
+  if (mouseY < circle.y) {
+    circle.ay = -circle.acceleration;
+  }
+  else {
+    circle.ay = circle.acceleration;
+  }
+
+  circle.vx = circle.vx + circle.ax;
+  circle.vx = constrain (circle.vx, -circle.maxSpeed, circle.maxSpeed);
+  circle.vy = circle.vy + circle.ay;
+  circle.vy = constrain (circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
 }
 
-function mousePressed() {
-  bg.r = random(0, 255);
-  bg.g = random(0, 255);
-  bg.b = random(0, 255);
-}
+// function mousePressed() {
+//   bg.r = random(0, 255);
+//   bg.g = random(0, 255);
+//   bg.b = random(0, 255);
+// }
 
 //   let d = dist(circle.x, circle.y, dangerZone.x, dangerZone.y);
 //   while (d < circle.size / 2 + dangerZone.size/2) {
