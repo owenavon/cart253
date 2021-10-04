0b15b6ba82dd18b59cb8fc6ef4ee488b5ecd09a0
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-04 19:40:57 -0400

    Started experimenting with functions

---
+++ experiments/functions-experiments/js/script.js
@@ -0,0 +1,17 @@
+
+// Experimenting with Conditionals
+// Owen Avon
+
+// Learning and experimenting with conditionals
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
