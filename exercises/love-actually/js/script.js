e8443e22d8c1118ac1271cf05c8a5d9271521d5f
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-06 15:48:41 -0400

    Started "Love, actually" exercise

---
+++ exercises/love-actually/js/script.js
@@ -0,0 +1,17 @@
+
+// Exercise 03: Love Actually
+// Owen Avon
+
+// This project focuses on...
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
