b7662489eccc6762cddd689d1b1c07acc9308458
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-06 14:42:26 -0400

    A5: Started the Looking for Love activity

---
+++ activities/05-looking-for-love/js/script.js
@@ -0,0 +1,17 @@
+
+// Activity 05: Looking for Love
+// Owen Avon
+
+// The pupose of this activity is to practice using all aquired JS knowledge thus far. 
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
