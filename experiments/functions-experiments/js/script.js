ffca43a3328d8651a9d8f03ef73a7bb4b4d1dd32
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-05 22:07:01 -0400

    Completed functions with return values

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -17,33 +17,49 @@
 // Description of setup()
 function setup() {
   createCanvas(500, 500);
+
+  let hotCelsius = toCelsius(100);
+  console.log(`100 degrees Farenheit is ${hotCelsius} degrees Celsius.`);
+
+  let coldCelsius = toCelsius(10);
+  console.log(`10 degrees Fahrenheit is ${coldCelsius} degrees Celsius.`);
 }
 
 // Description of draw()
 function draw() {
   background(0);
 
-  parallels (100, 100, 5, 1, 100, 1);
-  parallels (50, 50, 10, 2, 20, 10);
-  parallels (200, 200, 15, 7, 3, 20);
-  parallels (312, 257, 20, 0.5, 300, 2);
+  // let x = random(0, width);
+  // let y = random (0, height);
+  //
+  // ellipse(x, y, 100); // I can call a function where I want to use it's return value.
+
+  // parallels (100, 100, 5, 1, 100, 1);
+  // parallels (50, 50, 10, 2, 20, 10);
+  // parallels (200, 200, 15, 7, 3, 20);
+  // parallels (312, 257, 20, 0.5, 300, 2);
 }
 
-function parallels (x, y, numLines, lineWidth, lineHeight, lineSpacing) { // x and y parameters are defined in the function above.
-  // let x = 50;
-  // let y = 250;
-  for (let i = 0; i < numLines; i++) {
-    noStroke();
-    fill(255);
-    rectMode(CENTER);
-    rect(x, y, lineWidth, lineHeight);
-    x = x + lineSpacing;
+function toCelsius(fahrenheit) { // Function is called signature. Parameter
+  let celsius = (fahrenheit - 32) * 5/9; // Variable
+  return celsius;
 }
+
+// function parallels (x, y, numLines, lineWidth, lineHeight, lineSpacing) { // x and y parameters are defined in the function above.
+//   // let x = 50;
+//   // let y = 250;
+//   for (let i = 0; i < numLines; i++) {
+//     noStroke();
+//     fill(255);
+//     rectMode(CENTER);
+//     rect(x, y, lineWidth, lineHeight);
+//     x = x + lineSpacing;
+// }
 
   // move ();
   // wrap ();
   // display();
-}
+// }
 
 // function move () {
 //   circle.x = circle.x + circle.vx; // Means the circle moves
