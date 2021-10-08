fc4ee1e12ecff0f556f1debde8b7cdb030e87cb3
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-07 20:35:31 -0400

    A5: Completed "Love, actually" exercise. See note below.
    Completed organizing my code, setting functions and commenting.
    
    This exercise answered some many of my questions. I was happy to learn states and work with functions. I am overall happy with my exercise, and look forward to spending time on project 1.

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -2,15 +2,20 @@
 // Exercise 03: Love Actually
 // Owen Avon
 
-// The pupose of this exercise is to practice using all aquired JS knowledge thus far.
+// The purpose of this exercise is to create a program with various states and experiment with knowledge that we have gained thus far.
 
 //Plan
+// - Make one of the circles controlled by the mouse in a reversed direction.
+// - Apply the noise() function to the other circle to create a staggered movement effect.
+// - Add a timer and connect a state to it, so that when the timer is up, the player wins the game.
+// *- Add an "Easter Egg" state named "escape" which can be accessed when in the "simulation" state by pressing the "ESC" key.*
+// - Change the circles to 8-bit characters, add 8-bit backgrounds and change the font to an appropriate theme style.
 
 "use strict";
 
 let link = {
   x: undefined,
-  y: 700,
+  y: 600,
   size: 100,
   vx: 0,
   vy: 0,
@@ -19,7 +24,7 @@ let link = {
 
 let zelda = {
   x: undefined,
-  y: 250,
+  y: undefined,
   size: 100,
   minSize: 25,
   maxSize: 125,
@@ -36,7 +41,7 @@ let bgImages = {
   space: undefined,
   escape: undefined,
   winner: undefined,
-}
+};
 
 let characterImages = {
   x: 0,
@@ -44,184 +49,185 @@ let characterImages = {
   size: 125,
   link: undefined,
   zelda: undefined
-}
+};
+
+let fontSize = {
+  small: 32,
+  medium: 64,
+  large: 100
+};
 
 let eightBitFont;
-let timer = 20
-let state = `title`; // Can be: title, simulation, loser, winner, escape
+let timer = 15 // Sets the timer's value.
+let state = `title`; // Can be: title, simulation, loser, winner, escape.
 
-function preload() {
-  bgImages.grass = loadImage("assets/images/grass.png");
-  bgImages.space = loadImage("assets/images/space.gif");
-  bgImages.escape = loadImage("assets/images/escape.png");
-  bgImages.winner = loadImage("assets/images/winner.png");
-  characterImages.link = loadImage("assets/images/link.png");
-  characterImages.zelda = loadImage("assets/images/zelda.png");
+function preload() { // asynchronously loads files before the setup() function.
+  bgImages.grass = loadImage("assets/images/grass.png"); // Preloads the "Grass" image.
+  bgImages.space = loadImage("assets/images/space.gif"); // Preloads the "space" gif.
+  bgImages.escape = loadImage("assets/images/escape.png"); // Preloads the "escape" image.
+  bgImages.winner = loadImage("assets/images/winner.png"); // Preloads the "winner" image.
+  characterImages.link = loadImage("assets/images/link.png"); // Preloads the "link" character image.
+  characterImages.zelda = loadImage("assets/images/zelda.png"); // Preloads the "zelda" character image.
 
-  eightBitFont = loadFont("assets/font/8-bit-pusab.ttf")
+  eightBitFont = loadFont("assets/font/8-bit-pusab.ttf") // Preloads the custom downloaded font.
 }
 
-function setup() {
-  createCanvas(750, 750);
-  setupCircles();
+function setup () { // executes the lines of code contained inside its block.
+  createCanvas (750, 750); // Sets the Canvas width and height.
+  setupCharacters (); // Calls the setupCharacters() function.
 }
 
-function setupCircles () {
-  // Position circles sperated from one another.
-  link.x = width / 3;
-  zelda.x = 2 * width / 3;
+function setupCharacters () { // Dictates the two characters.
+  link.x = width / 3; // provides the static position of link.
+  zelda.x = 2 * width / 3; // Provides the static position of zelda.
 }
 
-function draw() {
-
-  if (state === `title`) {
+function draw() { // Location where code is excuted.
+  if (state === `title`) { // Indicates that when the state equates to "title", start said state.
     title();
   }
-  else if (state === `simulation` ) {
+  else if (state === `simulation` ) { // Indicates that when the state equates to "simulation", start said state.
     simulation ();
   }
-  else if (state === `loser`) {
+  else if (state === `loser`) { // Indicates that when the state equates to "loser", start said state.
     loser ();
   }
-
-  else if (state === `winner`) {
+  else if (state === `winner`) { // Indicates that when the state equates to "winner", start said state.
     winner ();
   }
-
-  else if (state === `escape`) {
+  else if (state === `escape`) { // Indicates that when the state equates to "escae", start said state.
     escape ();
   }
 }
 
-function title () {
-  push();
-  background(bgImages.space);
-  textSize(30);
-  fill(200, 100, 100);
-  textAlign(CENTER, CENTER);
-  textFont(eightBitFont);
-  text(`Can you avoid the enemy?`, width / 2, height / 2.75);
-  text(`Press "P" to play`, width / 2, height / 2);
-  pop();
+function title () { // Location where code is excuted.
+  push(); // Isolates code from using global properties.
+  background(bgImages.space); // Calls the background image.
+  textSize(fontSize.small); // Displays the font size as 32px.
+  fill(200, 100, 100); // Makes the font salmon in colour.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  textFont(eightBitFont); // Changes the font from the default to a custom font.
+  text(`Can you avoid the enemy?`, width / 2, height / 2.75); // Displays on screen text at desired location.
+  text(`Press "P" to play`, width / 2, height / 2); // Displays on screen text at desired location.
+  pop(); // Isolates code from using global properties.
 }
 
-function simulation () {
-  background(bgImages.grass);
-  circleOneControl();
-  circleTwoMovement();
-  gameTimer();
-  checkOverlap();
-  display();
+function simulation () { // simulation state.
+  background(bgImages.grass); // Calls the background image.
+  linkControl(); // Calls indciated custom function.
+  zeldaMovement(); // Calls indciated custom function.
+  gameTimer(); // Calls indciated custom function.
+  checkOverlap(); // Calls indciated custom function.
+  display(); // Calls indciated custom function.
 }
 
-function loser () {
-  push();
-  background (0);
-  textSize(100);
-  fill(255, 0, 0);
-  textFont(eightBitFont);
-  textAlign(CENTER, CENTER);
-  text(`GAME OVER`, keyCode, width / 2.6, height / 1.15);
-  pop();
+function loser () { // loser state.
+  push(); // Isolates code from using global properties.
+  background (0); // Sets the background to black in colour.
+  textSize(fontSize.large); // Displays the font size as 100px.
+  fill(255, 0, 0); // Makes the font red in colour.
+  textFont(eightBitFont); // Changes the font from the default to a custom font.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(`GAME OVER`, keyCode, width / 2.6, height / 1.15); // Displays on screen text at desired location.
+  pop(); // Isolates code from using global properties.
 }
 
-function winner () {
-  push();
-  background(bgImages.winner);
-  textSize(32);
-  fill(255);
-  textFont(eightBitFont);
-  textAlign(CENTER, CENTER);
-  text(`You've survived the enemy!`, keyCode, width / 2, height / 1.2);
-  pop();
+function winner () { // winner state.
+  push(); // Isolates code from using global properties.
+  background(bgImages.winner); // Calls the background image.
+  textSize(fontSize.small); // Displays the font size as 32px.
+  fill(255); // Makes the font white in colour.
+  textFont(eightBitFont); // Changes the font from the default to a custom font.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(`You've survived the enemy!`, keyCode, width / 2, height / 1.2); // Displays on screen text at desired location.
+  pop(); // Isolates code from using global properties.
 }
 
-function escape () {
-  push();
-  background(bgImages.escape);
-  fill(255);
-  textFont(eightBitFont);
-  textAlign(CENTER);
-  textSize(32);
-  text(`You've managed to flee`, keyCode, width / 5, height / 1.1);
-  pop();
+function escape () { // escape state.
+  push(); // Isolates code from using global properties.
+  background(bgImages.escape); // Calls the background image.
+  textSize(fontSize.small); // Displays the font size as 32px.
+  fill(255); // Makes the font white in colour.
+  textFont(eightBitFont); // Changes the font from the default to a custom font.
+  textAlign(CENTER, CENTER); // Dictates the text alignment style.
+  text(`You've managed to flee`, keyCode, width / 5, height / 1.1); // Displays on screen text at desired location.
+  pop(); // Isolates code from using global properties.
 }
 
-function circleOneControl () {
-  let dx = link.x - mouseX;
-  let dy = link.y - mouseY;
+function linkControl () { // Allows link to move via the users mouse position.
+  let dx = link.x - mouseX; // Defines x distance variable.
+  let dy = link.y - mouseY; // Defines y distance variable.
 
-  if (dx < 0) {
+  if (dx < 0) { // provides direction for left mouse movement.
     link.vx = -link.speed;
   }
-  else if (dx > 0) {
+  else if (dx > 0) { // provides direction for right mouse movement.
     link.vx = link.speed;
   }
-  if (dy < 0) {
+  if (dy < 0) { // provides direction for upward mouse movement.
     link.vy = -link.speed;
   }
-  else if (dy > 0) {
+  else if (dy > 0) { // provides direction for downward mouse movement.
     link.vy = link.speed;
   }
 
-  link.x = link.x + link.vx; // Allows circle 1 to move
-  link.y = link.y + link.vy;
+  link.x = link.x + link.vx; // Allows link to move on the x-axis
+  link.y = link.y + link.vy; // Allows link to move on the y-axis
 
-  link.x = constrain(link.x, 0, 655); // Constrian for circle 1
-  link.y = constrain(link.y, 0, 680);
+  link.x = constrain(link.x, 0, 655); // Constrains link's x postion within the canvas.
+  link.y = constrain(link.y, 0, 680); // Constrains link's y postion within the canvas.
 }
 
-function circleTwoMovement () { // Allows zelda to move in regards to noise
-  zelda.x = map(noise(zelda.tx), 0, 1, 0, width);
-  zelda.y = map(noise(zelda.ty), 0, 1, 0, height);
+function zeldaMovement () { // Allows zelda to move in regards to noise.
+  zelda.x = map(noise(zelda.tx), 0, 1, 0, width); // Creates a staggered effect for Zelda's x postion.
+  zelda.y = map(noise(zelda.ty), 0, 1, 0, height); // Creates a staggered effect for Zelda's y postion.
 
-  zelda.tx = zelda.tx + 0.02;
-  zelda.ty = zelda.ty + 0.02;
+  zelda.tx = zelda.tx + 0.025; // States the time between the staggered direction change on the x-axis.
+  zelda.ty = zelda.ty + 0.025; // States the time between the staggered direction change on the y-axis.
 
-  let x = zelda.x;
-  let numSegments = 5;
+  let x = zelda.x; // Sets a local variable for zelda's x postion.
+  let numSegments = 5; // Says I want 5 instances of zelda.
 
-  for (let i = 0; i < numSegments; i++) {
-    image(characterImages.zelda, x, zelda.y, zelda.segmentSize);
-    x = x + 75;
+  for (let i = 0; i < numSegments; i++) { // Allows the for() loop to add additional instance(s).
+    image(characterImages.zelda, x, zelda.y, zelda.segmentSize); // Indicates the variable that will be duplicated.
+    x = x + 80; // Provides 80 px distance between each instance of zelda.
   }
 }
 
-function gameTimer () { // REVIEW THIS and make a state arise
-  push();
-  fill(255);
-  textFont(eightBitFont);
-  textAlign(CENTER, TOP);
-  textSize(64);
-  text(timer, width / 5, height / 5.5);
-  pop();
+function gameTimer () { // Creates a dynamic game clock.
+  push(); // Isolates code from using global properties.
+  fill(255); // Makes the font white in colour.
+  textFont(eightBitFont); // Changes the font from the default to a custom font.
+  textAlign(CENTER, TOP); // Dictates the text alignment style.
+  textSize(fontSize.medium); // Displays the font size as 64px.
+  text(timer, width / 5, height / 5.5); // Displays on screen text at desired location.
+  pop(); // Isolates code from using global properties.
 
-  if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
+  if (frameCount % 60 == 0 && timer > 0) { // Indicates that if the frameCount is divisible by 60, then a second has passed.
     timer --;
   }
-  if (timer == 0) {
-    // WINNER ENDING
-    state = `winner`;
+  if (timer == 0) { // Stops when the timer hits zero, and...
+    state = `winner`; // Runs the winner state.
   }
 }
 
-function checkOverlap () {  // Check if the circles overlap
-  let d = dist(link.x, link.y, zelda.x, zelda.y);
-  if (d < link.size / 2 + zelda.size / 2) {
-    state = `loser`;
+function checkOverlap () {  // Checks to see if Zelda and Link touch.
+  let d = dist(link.x, link.y, zelda.x, zelda.y); // Assigns a variable to link and Zelda in regards to distance.
+  if (d < link.size / 2 + zelda.size / 2) { // Indicates the location of where the two characters will touch.
+    state = `loser`; // When the two characters touch, state will chnage to "loser".
   }
 }
 
-function display () {
-  image(characterImages.link, link.x, link.y, link.size); // Displays the link character
-  image(characterImages.zelda, zelda.x, zelda.y, zelda.size); // Displays the zelda character
+function display () { // Displays the on screen characters.
+  image(characterImages.link, link.x, link.y, link.size); // Displays the link character.
+  image(characterImages.zelda, zelda.x, zelda.y, zelda.size); // Displays the zelda character.
 }
 
-function keyPressed () {
-  if (keyCode === 80 && state === `title`) {
-    state = `simulation`;
+function keyPressed () { // p5 function to perform action with keyboard input.
+  if (keyCode === 80 && state === `title`) { // Says when the "P" key is pushed, and the state is in "title", switch to the "simulation" state.
+    state = `simulation`; // Runs the "simulation" state.
   }
-  else if (keyCode === 27 && state === `simulation`) {
-    state = `escape`;
+  else if (keyCode === 27 && state === `simulation`) { // Says when the "ESC" key is pushed, and the state is in "simulation", switch to the "escape" state.
+    state = `escape`; // Runs the "simulation" state.
   }
 }
