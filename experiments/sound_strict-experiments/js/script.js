ab14b98a5308d6c81eed213258622841d9a410e7
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-11 11:17:50 -0400

    Completed experimenting with Basic sound

--- experiments/sound_strict-experiments/js/script.js
+++ experiments/sound_strict-experiments/js/script.js
@@ -6,14 +6,32 @@
 
 "use strict";
 
+let music;
+
+function preload() {
+  music = loadSound(`assets/sounds/bark.wav`);
+}
+
 // Description of setup()
 function setup() {
   createCanvas(500, 500);
-
 }
 
 // Description of draw()
 function draw() {
   background(0);
+}
 
+function mousePressed() { // Cannot play music as the broswer loads. This code trys to get around the broswers regulations.
+  tryMusic();
+}
+
+function keyPressed() {
+  tryMusic();
+}
+
+function tryMusic() {
+  if (!music.isPlaying()) {
+    music.loop();
+  }
 }
