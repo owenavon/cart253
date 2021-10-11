b4b2c3d1e7d5a05ed0e8ad22ef9728aeb6448b1c
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-11 11:27:21 -0400

    Completed experimenting with "use strict"

--- experiments/sound_strict-experiments/js/script.js
+++ experiments/sound_strict-experiments/js/script.js
@@ -6,11 +6,17 @@
 
 "use strict";
 
-let music;
-
-function preload() {
-  music = loadSound(`assets/sounds/bark.wav`);
+let circle = {
+  x: 0,
+  y: 0,
+  size: 100
 }
+
+// let music;
+
+// function preload() {
+//   music = loadSound(`assets/sounds/bark.wav`);
+// }
 
 // Description of setup()
 function setup() {
@@ -20,18 +26,23 @@ function setup() {
 // Description of draw()
 function draw() {
   background(0);
+
+  circle.x = mouseX;
+  circle.y = mouseY;
+
+  ellipse(circle.x, circle.y, 100);
+
 }
 
-function mousePressed() { // Cannot play music as the broswer loads. This code trys to get around the broswers regulations.
-  tryMusic();
-}
-
-function keyPressed() {
-  tryMusic();
-}
-
-function tryMusic() {
-  if (!music.isPlaying()) {
-    music.loop();
-  }
-}
+// function mousePressed() { // Cannot play music as the broswer loads. This code trys to get around the broswers regulations.
+//   tryMusic();
+// }
+//
+// function keyPressed() {
+//   tryMusic();
+// }
+//
+// function tryMusic() {
+//   if (!music.isPlaying()) {
+//     music.loop();
+//   }
