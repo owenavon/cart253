e56fe09a1c6cfb4a20e1d486dd2b197e1f931246
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-11 12:08:30 -0400

    P1: Started project 1 - Acorn Catcher

---
+++ projects/acorn-catcher/js/script.js
@@ -0,0 +1,17 @@
+
+// Project 1 - Acorn Catcher
+// Owen Avon
+
+// This purpose of this program is to create a simulation in which the user must achieve a specific goal within a given amount of time. In this instance, the user will attempt to collect 100 acorn points within 30 seconds.
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
