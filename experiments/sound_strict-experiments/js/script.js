e9ccbaf79fedc4e874c0622f940ff21c6b2fbbb2
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-11 10:57:24 -0400

    Started experimenting with Basic sound

---
+++ experiments/sound_strict-experiments/js/script.js
@@ -0,0 +1,19 @@
+
+// Sound and Strict Experiments
+// Owen Avon
+
+// Learning and experimenting with sound and the "strict" function.
+
+"use strict";
+
+// Description of setup()
+function setup() {
+  createCanvas(500, 500);
+
+}
+
+// Description of draw()
+function draw() {
+  background(0);
+
+}
