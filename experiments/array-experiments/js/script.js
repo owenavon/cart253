596e55422ae34c91d069909da6566e7e4187f867
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-19 17:48:37 -0400

    Started experimenting with intermediate functions

---
+++ experiments/array-experiments/js/script.js
@@ -0,0 +1,40 @@
+
+// Experimenting with Arrays
+// Owen Avon
+
+// Learning and experimenting with arrays
+
+let user = {
+  x: 0,
+  y: 0,
+  size: 100
+};
+
+// First food object
+let food1 = {
+  x: 250,
+  y: 300,
+  size: 50,
+  eaten: false // We want to track eather the user has eaten the food
+};
+
+let food2 = {
+  x: 350,
+  y: 300,
+  size: 50,
+  alive: false
+};
+
+"use strict";
+
+// Description of setup()
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+// Description of draw()
+function draw() {
+  background(0);
+
+}
