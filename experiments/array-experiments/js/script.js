f17d1df8ca21df7f08c205009d6d0bccd7a441a4
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-19 19:07:00 -0400

    Completed experimenting with arrays

--- experiments/array-experiments/js/script.js
+++ experiments/array-experiments/js/script.js
@@ -4,147 +4,87 @@
 
 // Learning and experimenting with arrays
 
-let user = {
-  x: 0,
-  y: 0,
-  size: 100
-};
-
-// Foods
-let food1;
-let food2;
-let food3;
-let food4;
-let food5;
-let food6;
-
 "use strict";
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+let school = [];
+let schoolSize = 10;
 
-  food1 = createFood(250, windowHeight / 2);
-  food2 = createFood(350, windowHeight / 2);
-  food3 = createFood(450, windowHeight / 2);
-  food4 = createFood(550, windowHeight / 2);
-  food5 = createFood(650, windowHeight / 2);
-  food6 = createFood(750, windowHeight / 2);
+// Foods
+let fish1;
+let fish2;
+let fish3;
+let fish4;
+
+function setup() {
+  createCanvas(600, 600);
+
+  for (let i = 0; i < schoolSize; i++) {
+    let fish = createFish(random(0, width), random(0, height));
+    school.push(fish);
+    // school [i] = createFish(random(0, width), random(0, height)); // Same outcome
+  }
+
+  // school [0] = createFish(random(0, width), random(0, height)); // index 0, element at index 0
+  // school [1] = createFish(random(0, width), random(0, height)); // index 1, element at index 1
+  // school [2] = createFish(random(0, width), random(0, height)); // index 2, element at index 2
+  // school [3] = createFish(random(0, width), random(0, height)); // index 3, element at index 3
 }
 
-function createFood (x, y) {
-  let food = {
+function createFish (x, y) {
+  let fish = {
     x: x,
     y: y,
     size: 50,
-    eaten: false
+    vx: 0,
+    vy: 0,
+    speed: 2,
   };
-  return food;
+  return fish;
 }
-
 
 function draw() {
   background(0);
 
-  // Move the user (with the mouse)
-  moveUser();
+  for (let i = 0; i < school.length; i++ ) {
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
 
-  // Check whether the user has eaten either food
-  checkFood(food1);
-  checkFood(food2);
-  checkFood(food3);
-  checkFood(food4);
-  checkFood(food5);
-  checkFood(food6);
+  // moveFish(school[0]);
+  // moveFish(school[1]);
+  // moveFish(school[2]);
+  // moveFish(school[3]);
 
-  // Display the user and foods
-  displayUser();
-  displayFood(food1);
-  displayFood(food2);
-  displayFood(food3);
-  displayFood(food4);
-  displayFood(food5);
-  displayFood(food6);
+  // displayFish(school[0]);
+  // displayFish(school[1]);
+  // displayFish(school[2]);
+  // displayFish(school[3]);
 }
 
 // Sets the user position to the mouse position
-function moveUser() {
-  user.x = mouseX;
-  user.y = mouseY;
-}
-
-function displayFood (food) {
-  if(!food.eaten) {
-    push();
-    fill(255, 100, 100);
-    ellipse(food.x, food.y, food.size);
-    pop();
+function moveFish(fish) {
+  let change = random(0, 1);
+  if (change < 0.05) { // 5% chance that the fish change direction.
+    fish.vx = random (-fish.speed, fish.speed);
+    fish.vy = random (-fish.speed, fish.speed);
   }
+
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
 }
 
-// Draw the user as a circle
-function displayUser () {
+function displayFish (fish) {
   push();
-  fill(255);
-  ellipse(user.x, user.y, user.size);
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
   pop();
 }
 
-function checkFood (food) {
-  if(!food.eaten) {
-    let d = dist(user.x, user.y, food.x, food.y);
-    if (d < user.size / 2 + food.size / 2) {
-      food.eaten = true;
-    }
-  }
+function mousePressed() {
+  let fish = createFish(mouseX, mouseY);
+  school.push (fish);
 }
-
-// function displayFood1 () {
-//   if(!food1.eaten) {
-//     push ();
-//     fill (255, 100, 100);
-//     ellipse (food1.x, food1.y, food1.size);
-//   }
-// }
-
-// function displayFood2 () {
-//   if(!food2.eaten) {
-//     push ();
-//     fill (255, 100, 100);
-//     ellipse (food2.x, food2.y, food2.size);
-//   }
-// }
-
-// function displayFood3 () {
-//   if(!food3.eaten) {
-//     push ();
-//     fill (255, 100, 100);
-//     ellipse (food3.x, food3.y, food3.size);
-//   }
-// }
-
-// function checkFood1 () {
-//   if(!food1.eaten) {
-//     let d = dist(user.x, user.y, food1.x, food1.y);
-//     if (d < user.size / 2 + food1.size / 2) {
-//       food1.eaten = true;
-//     }
-//   }
-// }
-
-// function checkFood2 () {
-//   if(!food2.eaten) {
-//     let d = dist(user.x, user.y, food2.x, food2.y);
-//     if (d < user.size / 2 + food2.size / 2) {
-//       food2.eaten = true;
-//     }
-//   }
-// }
-
-// function checkFood3 () {
-//   if(!food3.eaten) {
-//     let d = dist(user.x, user.y, food3.x, food3.y);
-//     if (d < user.size / 2 + food2.size / 2) {
-//       food3.eaten = true;
-//     }
-//   }
-// }
