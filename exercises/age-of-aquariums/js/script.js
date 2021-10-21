6d1dc87c2e8c59aef5a2dafafb39b4fc17de8ee8
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-20 20:20:29 -0400

    Progressing on EX4: Age of Aquariums exercise. See note below.
    I have decided to remain relatively content with this program. I started with the fish simulation, and have added a shark in which the user will control with mouseX and mouseY. The objective will be to eat 500 points worth of fish within 20 seconds. So far the programming is going alright. I have stumbled upon an error when attempting to target an objects variables inside of an array.... My question has been sent on the discord!

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -16,25 +16,49 @@
 let school = [];
 let schoolSize = 10;
 
-// Foods
-let fish1;
-let fish2;
-let fish3;
-let fish4;
+let shiver = [];
+let shiverSize = 1;
+
+// Shark
+let shark = {
+  x: 100,
+  y: 100,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 1,
+}
+
+let gameTitle = {
+  string: `Acorn Catcher`,
+  x: 375,
+  y: 0,
+  vx: 0,
+  vy: 2,
+}
+
+let gameStart = {
+  string: `Press "Enter" to play`,
+  x: 0,
+  y: 375,
+  vx: 2.5,
+  vy: 0,
+}
+
+let score = 0; // Starts the score board at "0".
+let timer = 25; // Sets the timer's value.
+let state = `landing`; // Provides the starting state. Can be "landing", "simulation", "winner", "loser".
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(750, 750);
+  setupFish ();
+}
 
+function setupFish () {
   for (let i = 0; i < schoolSize; i++) {
     let fish = createFish(random(0, width), random(0, height));
     school.push(fish);
-    // school [i] = createFish(random(0, width), random(0, height)); // Same outcome
   }
-
-  // school [0] = createFish(random(0, width), random(0, height)); // index 0, element at index 0
-  // school [1] = createFish(random(0, width), random(0, height)); // index 1, element at index 1
-  // school [2] = createFish(random(0, width), random(0, height)); // index 2, element at index 2
-  // school [3] = createFish(random(0, width), random(0, height)); // index 3, element at index 3
 }
 
 function createFish (x, y) {
@@ -45,30 +69,114 @@ function createFish (x, y) {
     vx: 0,
     vy: 0,
     speed: 2,
-  };
+    eaten: false
+  }
   return fish;
 }
 
 function draw() {
-  background(0);
+  if (state === `landing`) { // Indicates that when the state equates to "landing", start said state.
+    landing();
+  }
+  else if (state === `simulation` ) { // Indicates that when the state equates to "simulation", start said state.
+    simulation ();
+  }
+  else if (state === `winner`) { // Indicates that when the state equates to "winner", start said state.
+    winner ();
+  }
+  else if (state === `loser`) { // Indicates that when the state equates to "loser", start said state.
+    loser ();
+  }
+}
 
+function landing () { // Main landing state code.
+  push (); // Isolates code from using global properties.
+  background (0); // Displays the background colour as black.
+  // textSize (fontSize.medium); // Displays the font size as 32px.
+  fill (255); // Makes the font white in colour.
+  textAlign (CENTER, CENTER); // Dictates the text alignment style.
+  // textFont (retroFont); // Changes the font from the default to a custom font.
+  text (gameTitle.string, gameTitle.x, gameTitle.y); // Displays the title of the game.
+  text (gameStart.string, gameStart.x, gameStart.y); // Displays the text that dictates what the user must press to start the game.
+  pop (); // Isolates code from using global properties.
+}
+
+function simulation () { // simulation state.
+  background(0); // Calls the background image.
+
+  controlShark();
+  displayShark(); // Calls indciated custom function.
+
+  drawFish();
+
+  sharkEatFish();
+
+  gameTimer();
+  scoreBoard ();
+}
+
+function winner () { // Main winner state code.
+  push(); // Isolates code from using global properties.
+  background (0); // Displays the background colour as black.
+  // textSize (fontSize.large); // Displays the font size as 32px.
+  fill (255); // Makes the font yellow in colour.
+  // textFont (retroFont); // Changes the font from the default to a custom font.
+  textAlign (CENTER, CENTER); // Dictates the text alignment style.
+  winnerText (); // Calls the winnerText function to display the text.
+  text (`Congratulations!`, width / 2, height / 2); // // Displays on screen text the top left corner. It is then translated via the parameter above.
+  pop (); // Isolates code from using global properties.
+}
+
+function loser () { // Main loser state code.
+  push (); // Isolates code from using global properties.
+  background (0); // Displays the background colour as black.
+  // textSize (fontSize.large); // Displays the font size as 100px.
+  fill (255); // Makes the font red in colour.
+  // textFont (retroFont); // Changes the font from the default to a custom font.
+  textAlign (CENTER, CENTER); // Dictates the text alignment style.
+  text (`GAME OVER`, width / 2, height / 2); // Displays on screen text at the center of the canvas
+  pop (); // Isolates code from using global properties.
+}
+
+function controlShark () {
+  let dx = shark.x - mouseX; // Defines x distance variable.
+  let dy = shark.y - mouseY; // Defines y distance variable.
+
+  if (dx < 0) { // provides direction for left mouse movement.
+    shark.vx = shark.speed;
+  }
+  else if (dx > 0) { // provides direction for right mouse movement.
+    shark.vx = -shark.speed;
+  }
+  if (dy < 0) { // provides direction for upward mouse movement.
+    shark.vy = shark.speed;
+  }
+  else if (dy > 0) { // provides direction for downward mouse movement.
+    shark.vy = -shark.speed;
+  }
+
+  shark.x = shark.x + shark.vx;
+  shark.y = shark.y + shark.vy;
+
+  shark.x = constrain(shark.x, 0, width);
+  shark.y = constrain(shark.y, 0, height);
+}
+
+function displayShark () {
+  push();
+  fill(0, 0, 255);
+  noStroke();
+  ellipse(shark.x, shark.y, shark.size);
+  pop();
+}
+
+function drawFish () {
   for (let i = 0; i < school.length; i++ ) {
     moveFish(school[i]);
     displayFish(school[i]);
   }
-
-  // moveFish(school[0]);
-  // moveFish(school[1]);
-  // moveFish(school[2]);
-  // moveFish(school[3]);
-
-  // displayFish(school[0]);
-  // displayFish(school[1]);
-  // displayFish(school[2]);
-  // displayFish(school[3]);
 }
 
-// Sets the user position to the mouse position
 function moveFish(fish) {
   let change = random(0, 1);
   if (change < 0.05) { // 5% chance that the fish change direction.
@@ -84,14 +192,57 @@ function moveFish(fish) {
 }
 
 function displayFish (fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
+  if (!fish.eaten) {
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size);
+    pop();
+  }
 }
 
-function mousePressed() {
-  let fish = createFish(mouseX, mouseY);
-  school.push (fish);
+function sharkEatFish (fish) {
+  if (!fish.eaten) {
+    let d = dist(shark.x, shark.y, fish.x, fish.y);
+    if (d < shark.size / 2 + fish.size / 2) {
+      fish.eaten = true;
+      }
+    }
+  }
+
+function keyPressed () { // p5 function to perform action with keyboard input.
+  if (keyCode === 13 && state === `landing`) { // Says when the "Enter" key is pushed, and the state is in "landing", switch to the "simulation" state.
+    state = `simulation`; // Runs the "simulation" state.
+  }
+}
+
+function gameTimer () { // Main code for dynamic game clock.
+  push (); // Isolates code from using global properties.
+  // textSize (fontSize.medium); // Displays the font size as 64px.
+  fill (255); // Makes the font white in colour.
+  // textFont (retroFont); // Changes the font from the default to a custom font.
+  textAlign (CENTER, CENTER); // Dictates the text alignment style.
+  text (`Time:`, 600, 50); // Displays text at the top right of the canvas.
+  text (timer, 690, 50); // Displays dynamic timer result at the top right of the canvas.
+  pop (); // Isolates code from using global properties.
+
+  if (frameCount % 60 == 0 && timer > 0) { // Indicates that if the frameCount is divisible by 60, then a second has passed.
+    timer --;
+  }
+  if (timer == 0) { // If the timer hits zero (0), then...
+    state = `loser`; // Run the loser state.
+  }
+}
+
+function scoreBoard () { // Main code for dynamic score board.
+  push (); // Isolates code from using global properties.
+  fill (255); // Makes the font black in colour.
+  // textSize (fontSize.medium); // Displays the font size as 64px.
+  // textFont (retroFont); // Changes the font from the default to a custom font.
+  text (`Score:`, 50, 75); // Displays text at the top left of the canvas.
+  text (score, 190, 75); // Displays dynamic score result at the top left of the canvas.
+  pop (); // Isolates code from using global properties.
+  if (score >= 500) { // If the score is equal to or greater then 500...
+    state = `winner`; // Runs the winner state.
+  }
 }
