7adb29b0b02e577f24da7def0adc63840281429e
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-25 18:24:49 -0400

    Started experimenting with Object Oriented Programming

---
+++ experiments/object-oriented-experiments/js/script.js
@@ -0,0 +1,17 @@
+
+// Experimenting with Object-Oriented Programming
+// Owen Avon
+
+// Learning and experimenting with Object-Oriented Programming
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
