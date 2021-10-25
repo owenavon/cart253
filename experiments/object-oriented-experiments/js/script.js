1c2b6bbc4b8711c4534a0a03535f1d030505ff58
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-25 19:05:35 -0400

    Completed experimenting with Object Oriented Programming

--- experiments/object-oriented-experiments/js/script.js
+++ experiments/object-oriented-experiments/js/script.js
@@ -1,4 +1,3 @@
-
 // Experimenting with Object-Oriented Programming
 // Owen Avon
 
@@ -6,12 +5,44 @@
 
 "use strict";
 
-// Description of setup()
-function setup() {
+// Our garden
+let garden = {
+  // An array to store the individual flowers
+  flowers: [],
+  // How many flowers in the garden
+  numFlowers: 20,
+  // The color of the grass (background)
+  grassColor: {
+    r: 120,
+    g: 180,
+    b: 120
+  }
+};
 
+// setup() creates the canvas and the flowers in the garden
+function setup() {
+  createCanvas(600, 600);
+
+  // Create our flowers by counting up to the number of the flowers
+  for (let i = 0; i < garden.numFlowers; i++) {
+    // Create a new flower
+    let flower = new Flower(); // used to call constructor NEW
+    // let flower = createFlower();
+    // Add the flower to the array of flowers
+    garden.flowers.push(flower);
+  }
 }
 
-// Description of draw()
+// draw()
+// Displays our flowers
 function draw() {
+  // Display the grass
+  background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
 
+  // Loop through all the flowers in the array and display them
+  for (let i = 0; i < garden.flowers.length; i++) {
+    let flower = garden.flowers[i];
+    flower.display(); // NEW
+    // displayFlower(flower);
+  }
 }
