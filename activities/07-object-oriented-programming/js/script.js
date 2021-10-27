5c2433b923801b30b5d16ac8aac5092203a482ef
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-26 20:18:29 -0400

    A7: Completed the "Object-Oriented Programming activity"

--- activities/07-object-oriented-programming/js/script.js
+++ activities/07-object-oriented-programming/js/script.js
@@ -1,17 +1,46 @@
 
-// Experimenting with Conditionals
+// A7: Object-Oriented Programming activity
 // Owen Avon
 
-// Learning and experimenting with conditionals
+// Experimenting with Object-Oriented Programming
 
 "use strict";
 
+
+let gravityForce = 0.0025;
+let paddle;
+
+let balls = [];
+let numBalls = 10;
+
 // Description of setup()
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  paddle = new Paddle(300, 20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0, width);
+    let y = random(-400, -100);
+    let ball = new Ball(x, y);
+    balls.push(ball);
+  }
 }
 
 // Description of draw()
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
+
+  for (let i = 0; i < balls.length; i++) {
+    let ball = balls[i];
+    if (ball.active) {
+      ball.gravity(gravityForce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
 }
