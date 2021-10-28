38f3a51a30c464ce526902c55604166b6f57fbf4
owenavon <89999663+owenavon@users.noreply.github.com>
2021-10-28 13:34:10 -0400

    October 28 live coding session

---
+++ experiments/october-28/js/script.js
@@ -0,0 +1,18 @@
+
+// October 28th
+// Owen Avon
+
+// Live coding session
+
+"use strict";
+
+// Description of setup()
+function setup() {
+  createCanvas(500, 500);
+  reset();
+}
+
+// Description of draw()
+function draw() {
+  background(0);
+}
