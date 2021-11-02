810564e3c6724a9392f4e1012c4277b510efdd78
owenavon <89999663+owenavon@users.noreply.github.com>
2021-11-01 22:31:33 -0400

    Started experimenting with Inheritance

---
+++ experiments/inheritance-and-polymorphism-experiments/js/script.js
@@ -0,0 +1,16 @@
+// Experimenting with inheritance and polymorphism
+// Owen Avon
+
+// Learning and experimenting with inheritance and polymorphism
+
+"use strict";
+
+// Description of setup()
+function setup() {
+
+}
+
+// Description of draw()
+function draw() {
+
+}
