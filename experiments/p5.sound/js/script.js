bb45af85ad20584eaf29c3e5e04c8fe24de80085
owenavon <89999663+owenavon@users.noreply.github.com>
2021-11-09 14:54:12 -0500

    Completed experimenting with the p5.PolySynth class

--- experiments/p5.sound/js/script.js
+++ experiments/p5.sound/js/script.js
@@ -5,49 +5,44 @@
 
 "use strict";
 
-let oscillator;
-// let angle = 0;
+let synth;
+let notes = [`F2`, `G2`, `Ab3`, `Bb3`, `C3`, `Db3`, `Eb3`, `F3`]; // F-minor
+let currentNote = 0;
 
-let t = 0;
+// let oscillator;
+// let angle = 0;
+// let t = 0;
 
 // Description of setup()
 function setup() {
   createCanvas(600, 600);
-  userStartAudio(); // Good practice to include when playing sounds within a program
 
-  oscillator = new p5.Oscillator(440, `sine`);
-  oscillator.amp(0.2);
+  synth = new p5.PolySynth();
+
+  userStartAudio(); // Good practice to include when playing sounds within a program
 }
 
 // Description of draw()
 function draw() {
   background(0);
 
-  // let sinAngle = sin(angle); // Calculate the sin of current angle (0).
-  let noiseValue = noise(t); // Create a time step. Oscilaties in an orgnic way. Plays between the values of 0 and 1. Calclates at 0.
-  let newFreq = map(noiseValue, 0, 1, 20, 2000); // Map frequency to angle.
-  oscillator.freq(newFreq);
-
-  t = t + 0.05; // Inrease t everytime
-
-  // angle = angle + 0.1;
-
-  // let newFreq = map(mouseY, height, 0, 20, 20000);
-  // push();
-  // textSize(32);
-  // textAlign(LEFT, CENTER);
-  // fill(255);
-  // text(newFreq, 100, height / 2);
-  // pop();
-
-  // let newAmp = map(mouseX, 0, width, 0, 1);
-  // oscillator.amp(newAmp);
 }
 
-function mousePressed() {
-  oscillator.start(); // signalling to now be able to play
+function keyPressed() {
+  // Start the ghost player
+  setInterval(playRandomNote, 500);
 }
 
-function mouseReleased() {
-  oscillator.stop();
+function playRandomNote() {
+  let note = notes[currentNote];
+  synth.play(note, 1, 0, 0.3);
+
+  currentNote = currentNote + 1;
+  if (currentNote === notes.length) {
+    currentNote = 0;
+  }
 }
+
+// function mouseReleased() {
+//   oscillator.stop();
+// }
